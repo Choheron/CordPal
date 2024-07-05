a2aa46aaf11d19096402dc685295fd14bc7bf4ea
Thomas Campbell <trc527@gmail.com>
2024-07-05 08:50:39 -0400

    Fix tailwind conflicts

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
       </p>
       <Conditional showWhen={!(await isMember())}>
         <br/>
-        <p className="b pt-10 bg-gradient-to-b from-red-200 pb-6 pt-8 backdrop-blur-2xl dark:border-red-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="b pt-10 bg-gradient-to-b from-red-200 pb-6 backdrop-blur-2xl dark:border-red-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <div className="flex w-full justify-center">
             <svg className="h-56 w-56 text-red-600"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01"/>
