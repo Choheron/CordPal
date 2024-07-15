f7268f77b0e0ac6537bdbd4a2a59183e98414b34
Thomas Campbell <trc527@gmail.com>
2024-07-15 16:43:03 -0400

    Security Patch

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { verifyAuth } from './app/lib/discord_utils';
+import { verifyAuth, isMember } from './app/lib/discord_utils';
  
 export async function middleware(request: NextRequest) {
-  console.log("Checking login");
   // Check if user is authorized, if not, redirect to login
+  console.log("Ensuring user is logged in...");
   if(!(await verifyAuth())) {
     return NextResponse.redirect(new URL('/', request.url));
+  }
+  // Check if user is a member of the server, if not, redirect to homepage (As a way of ensuring no one mantually enters a url)
+  // NOTE: This should run only if on a page that isnt publically accessible
+  if((!request.nextUrl.pathname.match("/dashboard")) && (!await isMember())) {
+    console.log("Ensuring user is member of server and is allowed to view the link...")
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
   return;
 }
