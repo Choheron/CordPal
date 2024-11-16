15b1011809ee68da2c3631042d45bc06bfc3e96c
Thomas Campbell <trc527@gmail.com>
2024-11-16 17:30:51 -0600

    Add preview listening

--- frontend/src/app/ui/dashboard/spotify/top_songs_list.tsx
+++ frontend/src/app/ui/dashboard/spotify/top_songs_list.tsx
@@ -44,6 +44,7 @@ export default function TopSongsBox(props) {
             const artist_link = song_obj['artists'][0]['external_urls']['spotify']
             const song_name = song_obj['name']
             const song_link = song_obj['external_urls']['spotify']
+            const song_preview_url = song_obj['preview_url']
             
 
             return (
@@ -59,16 +60,16 @@ export default function TopSongsBox(props) {
                 className="-ml-4"
               >
                 <Card 
-                  className="h-28 w-full max-w-[640px]"
+                  className="h-40 2xl:h-28 w-full max-w-[640px]"
                 >
                   <CardHeader className="absolute my-auto z-10 top-1 flex !items-start">
                     <Avatar 
                       src={album_img_src}
                       className="w-20 h-20 min-w-20 max-w-20 text-large"
                     />
-                    <div className="flex flex-col ml-4">
-                      <h4 className="w-full font-bold text-large truncate">{song_name}</h4>
-                      <p className="text-tiny">
+                    <div className="flex flex-col ml-4 w-full">
+                      <h4 className="font-bold text-large line-clamp-1">{song_name}</h4>
+                      <p className="text-tiny line-clamp-1">
                         <a href={artist_link} target="_noreferrer" className="w-fit hover:underline">
                           {artist_name}
                         </a>
@@ -77,8 +78,16 @@ export default function TopSongsBox(props) {
                           {album_name}
                         </a>
                       </p>
-                      <small className="text-default-500"><a href={song_link} target="_noreferrer" className="underline hover:text-purple-400 w-fit text-sm">Listen</a></small>
-                      <small className="text-default-500">Released: {album_release_date}</small>
+                      <div className="flex flex-col 2xl:flex-row w-full justify-between">
+                        <div className="flex flex-col w-fit text-nowrap">
+                          <small className="text-default-500"><a href={song_link} target="_noreferrer" className="underline hover:text-purple-400 w-fit text-sm">Spotify</a></small>
+                          <small className="text-default-500">{album_release_date}</small>
+                        </div>
+                        <audio controls className="pt-2 pb-2 pl-2 w-64 lg:w-full">
+                          <source src={song_preview_url} type="audio/mpeg"/>
+                          Your browser does not support the audio element.
+                        </audio>
+                      </div>
                     </div>
                   </CardHeader>
                   <Image
