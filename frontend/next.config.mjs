4dba74bd6e1cc3176f640b6e63423824249f1429
Thomas Campbell <trc527@gmail.com>
2024-11-26 08:35:55 -0600

    Update Comments

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['i.scdn.co'], // Add your allowed domains here
+    domains: ['i.scdn.co'], // Add allowed domains here
   },
 };
 
