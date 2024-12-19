797d212c05742882b8e4c23a30d511a0dcaaae4b
Thomas Campbell <trc527@gmail.com>
2024-12-19 09:36:42 -0600

    Add functionality for video embeds

--- frontend/src/app/ui/dashboard/spotify/review_display.tsx
+++ frontend/src/app/ui/dashboard/spotify/review_display.tsx
@@ -12,6 +12,21 @@ import { getReviewsForAlbum } from "@/app/lib/spotify_utils";
 export default async function ReviewDisplay(props) {
   // Setup Props
   const reviews = await getReviewsForAlbum(props.album_id)
+  // Regex for youtube video embedding (Thanks GPT)
+  const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:\?[\w=&%-]*)?(?:&t=(\d+h)?(\d+m)?(\d+s)?)?/g;
+
+  const generateEmbed = (textWithLinks) => {
+    return textWithLinks.replace(youtubeRegex, (match, videoId, hours, minutes, seconds) => {
+      // Convert timestamp to seconds
+      const h = hours ? parseInt(hours) * 3600 : 0;
+      const m = minutes ? parseInt(minutes) * 60 : 0;
+      const s = seconds ? parseInt(seconds) : 0;
+      const startTime = h + m + s;
+
+      const startParam = startTime > 0 ? `?start=${startTime}` : '';
+      return `<iframe width="300" height="168.75" src="https://www.youtube.com/embed/${videoId}${startParam}" frameborder="0" allowfullscreen></iframe>`;
+    })
+  };
 
   return (
     <div className="w-full lg:w-fit min-w-[300px] mx-2 lg:mx-1 my-2 flex flex-col gap-2">
@@ -49,7 +64,7 @@ export default async function ReviewDisplay(props) {
                   <p className="ml-2 mr-auto">
                     <b>Comment:</b>
                   </p>
-                  <p className="mx-2 my-2 max-w-[320px]">{review['comment']}</p>
+                  <div className="mx-2 my-2 max-w-[320px]" dangerouslySetInnerHTML={{__html: generateEmbed(review['comment'])}} />
                   <p className="mx-2 my-2 align-middle">
                     Submitted: {convertToLocalTZString(new Date(review['review_date']), true)}
                   </p>
