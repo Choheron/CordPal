e050858fadead89f8150366221be7ad551846d94
Thomas Campbell <trc527@gmail.com>
2025-01-10 15:06:08 -0600

    Add placeholder for profile page

---
+++ frontend/src/app/profile/[userid]/page.tsx
@@ -0,0 +1,11 @@
+import PageTitle from "@/app/ui/dashboard/page_title";
+
+
+export default function Page() {
+
+  return (
+    <div className="flex min-h-screen flex-col items-center p-24 pt-10">
+      <PageTitle text="<NAME>'s Profile" />
+    </div>
+  );
+}
