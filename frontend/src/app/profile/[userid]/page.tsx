5def887059cabd903519930ba01aff4beaee962a
Thomas Campbell <trc527@gmail.com>
2025-01-21 12:38:18 -0600

    First steps at profile

--- frontend/src/app/profile/[userid]/page.tsx
+++ frontend/src/app/profile/[userid]/page.tsx
@@ -1,11 +1,20 @@
+import { getUserData } from "@/app/lib/user_utils";
 import PageTitle from "@/app/ui/dashboard/page_title";
 
-
-export default function Page() {
+// Profile Page
+// Route URL: Use GUID from system backend (NOT DISCORD ID)
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ userID: string }>
+}) {
+  const userid = (await params).userID
+  // Retreive data from backend
+  const userData = await getUserData(userid);
 
   return (
-    <div className="flex min-h-screen flex-col items-center p-24 pt-10">
-      <PageTitle text="<NAME>'s Profile" />
-    </div>
+    <main className="flex min-h-screen flex-col items-center p-24 pt-10">
+      <PageTitle text={`${userData['nickname']}'s Profile`} />
+    </main>
   );
 }
