eda484c0aac0bb9bae6b16ed53d59567a36c0351
Thomas Campbell <trc527@gmail.com>
2025-02-14 09:50:21 -0600

    Update verbiage

--- frontend/src/app/ui/dashboard/live_online_users_box.tsx
+++ frontend/src/app/ui/dashboard/live_online_users_box.tsx
@@ -9,10 +9,10 @@ export default function LiveOnlineUsersBox(props) {
   const userList = props.userList
 
   return(
-    <div className="flex flex-row px-2 py-2 items-center border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
+    <div className="flex flex-row px-2 py-2 min-w-[225px] items-center border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
       <div className="flex flex-col gap-1 h-full">
-        <p className="mb-2">
-          Other users that have validated:
+        <p className="mb-2 mx-auto">
+          Live Status of Users:
         </p>
         { 
           userList.sort((a, b) => (a['last_request_timestamp'] < b['last_request_timestamp']) ? 1 : -1).map((userObj, index) => {
