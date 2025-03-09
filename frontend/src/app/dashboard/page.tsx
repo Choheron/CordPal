02d5198cee6027b60e3f2db28317d271236b071d
Thomas Campbell <trc527@gmail.com>
2025-03-09 12:01:22 -0500

    CSS fixes

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -8,8 +8,6 @@ import LiveOnlineUsersBox from "../ui/dashboard/live_online_users_box";
 import CurrentTime from "../ui/general/current_time";
 import AlbumDisplay from "../ui/dashboard/spotify/album_display";
 import { getAlbumOfTheDayData } from "../lib/spotify_utils";
-import Link from "next/link";
-import { Button } from "@nextui-org/react";
 
 export default async function Page() {
   const discordUserData = await getDiscordUserData();
@@ -32,7 +30,7 @@ export default async function Page() {
 
   
   return (
-    <main className="flex min-h-screen flex-col items-center lg:p-24 pt-10">
+    <main className="flex min-h-screen flex-col items-center lg:p-24 pt-10 lg:pt-10">
       <PageTitle text="Homepage" />
       <div className="flex flex-col w-full lg:justify-center gap-3 lg:w-10/12 lg:flex-row" >
         <div className="flex flex-col w-fit">
@@ -40,7 +38,7 @@ export default async function Page() {
         </div>
         <div className="flex flex-col h-fit w-full max-w-[800px] px-2 py-2 lg:p-4 items-center border-neutral-800 bg-zinc-800/30 from-inherit rounded-xl border">
           <p className="text-2xl pb-2 underline font-extralight">
-            Today's Album of the Day:
+            Today&apos;s Album of the Day:
           </p>
           <AlbumDisplay 
             title={albumData("title")}
