803b3609b487187d6497d7b6bc743e2b481aa249
Thomas Campbell <trc527@gmail.com>
2025-06-03 18:19:23 -0500

    prep for future items

--- frontend/src/app/dashboard/aotd/album/[albumid]/page.tsx
+++ frontend/src/app/dashboard/aotd/album/[albumid]/page.tsx
@@ -84,7 +84,7 @@ export default async function Page({
     <div className="flex flex-col items-center p-3 pb-36 pt-10">
       <PageTitle text={`${albumData("title")}`} />
       <div className="flex flex-col w-fit justify-center md:w-4/5 gap-2">
-        <div className="flex mx-auto">
+        <div className="flex mx-auto gap-2">
           <div className="relative w-fit lg:max-w-[1080px] flex flex-col gap-2 lg:flex-row backdrop-blur-2xl px-2 py-2 my-2 rounded-2xl bg-zinc-800/30 border border-neutral-800">
             <AlbumDisplay 
               title={albumData("title")}
