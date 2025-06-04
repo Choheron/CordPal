74621aac39dd297e1ce931e24a58572792eecd02
Thomas Campbell <trc527@gmail.com>
2025-06-03 21:18:58 -0500

    Finalize signals

--- backend/aotd/apps.py
+++ backend/aotd/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class AotdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aotd'
+  default_auto_field = 'django.db.models.BigAutoField'
+  name = 'aotd'
+
+  def ready(self):
+    import aotd.signals
