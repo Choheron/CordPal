66de50e8ad9ba0fe750d053584ddc819fdc4b379
Thomas Campbell <trc527@gmail.com>
2025-08-06 12:55:58 -0500

    Apply baindaid for photo order on mobile

--- frontend/src/app/dashboard/photos/page.tsx
+++ frontend/src/app/dashboard/photos/page.tsx
@@ -1,10 +1,8 @@
-import PhotoModal from "@/app/ui/dashboard/photos/photo_modal";
+import { getAllArtists, getAllUploaders, getPhotoshops } from "@/app/lib/photos_utils";
+import PhotoFilterBlock from "@/app/ui/dashboard/photos/photo_filter_block";
+import PhotoGallery from "@/app/ui/dashboard/photos/image_gallery";
 import PageTitle from "@/app/ui/dashboard/page_title";
 import UploadPhotoModal from "@/app/ui/dashboard/photos/upload_photo_modal";
-
-import { getAllArtists, getAllUploaders, getPhotoshops } from "@/app/lib/photos_utils";
-import UserDropdown from "@/app/ui/general/userUiItems/user_dropdown";
-import PhotoFilterBlock from "@/app/ui/dashboard/photos/photo_filter_block";
 
 export default async function photos({searchParams}) {
   // Get url params
@@ -12,49 +10,6 @@ export default async function photos({searchParams}) {
   // Make requests to bakend to get uploader list and artist list
   const uploaderList = await getAllUploaders();
   const artistList = await getAllArtists();
-
-  // Load Images
-  async function loadImages() {
-    const fileListString: any = await getPhotoshops(uploader, artist, tagged);
-    if(fileListString.length == 0) {
-      return (<p>No Photos meet Filter Criteria</p>)
-    }
-    const fileList = fileListString.split(',')
-    // Cut list into 3 different columns (into a terribly named var)
-    var fileListList: any[] = [[],[],[]]
-    for (let i = 0; i < fileList.length; i++) {
-      // Show images in reverse order (newest uploads first)
-      const photoindex = (fileList.length - (i + 1))
-      // Show images newest from top left to bottom right
-      const listIndex = (i - 1) % 3
-      if(fileList[photoindex] == "") {
-        continue
-      }
-      if(listIndex == 0) {
-        fileListList[0].push(fileList[photoindex])
-      }else if(listIndex == 1) {
-        fileListList[1].push(fileList[photoindex])
-      } else {
-        fileListList[2].push(fileList[photoindex])
-      }
-    }
-
-    return (
-      <div className="flex flex-col lg:flex-row gap-6 w-full 2xl:w-3/4">
-        { fileListList.map((list: string[], listIndex: number) => (
-          <div key={listIndex} className="w-full flex flex-col gap-6 items-center pt-3">
-            { list.map((id: string, index: number) => (
-              <PhotoModal
-                key={id}
-                imageSrc={`/dashboard/photos/api/get-image/${id}`}
-                imageID={id}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  }
 
   return (
     <main className="flex flex-col items-center lg:px-24 pt-10">
@@ -67,7 +22,9 @@ export default async function photos({searchParams}) {
         artistList={artistList}
         //tagged={tagged}
       />
-      {loadImages()}
+      <PhotoGallery 
+        photos={await getPhotoshops(uploader, artist, tagged)}
+      />
     </main>
   );
 }
---
+++ frontend/src/app/ui/dashboard/photos/image_gallery.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import PhotoModal from "./photo_modal"
+
+export default function PhotoGallery(props) {
+  const fileListString: any = props.photos
+  // Track screen width
+  const [windowWidth, setWindowWidth] = useState(0);
+  // Return basic string if no photos found
+  if(fileListString.length == 0) {
+    return (<p>No Photos meet Filter Criteria</p>)
+  }
+
+  useEffect(() => {
+    // Get Window Width
+    if (typeof window !== "undefined") {
+      setWindowWidth(window.innerWidth)
+    }
+  }, [])
+  
+  const loadImages = () => {
+    const fileList = fileListString.split(',')
+    // Cut list into 3 different columns (into a terribly named var)
+    var fileListList: any[] = [[],[],[]]
+    for (let i = 0; i < fileList.length; i++) {
+      // Show images in reverse order (newest uploads first)
+      const photoindex = (fileList.length - (i + 1))
+      var listIndex = 0
+      if(windowWidth >= 1024) {
+        // Show images newest from top left to bottom right (Otherwise put them all in the same list)
+        listIndex = (i - 1) % 3
+      }
+      if(fileList[photoindex] == "") {
+        continue
+      }
+      if(listIndex == 0) {
+        fileListList[0].push(fileList[photoindex])
+      }else if(listIndex == 1) {
+        fileListList[1].push(fileList[photoindex])
+      } else {
+        fileListList[2].push(fileList[photoindex])
+      }
+    }
+
+    return (
+      <div className="flex flex-col lg:flex-row gap-6 w-full 2xl:w-3/4">
+        { fileListList.map((list: string[], listIndex: number) => (
+          <div key={listIndex} className="w-full flex flex-col gap-6 items-center pt-3">
+            { list.map((id: string, index: number) => (
+              <PhotoModal
+                key={id}
+                imageSrc={`/dashboard/photos/api/get-image/${id}`}
+                imageID={id}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  return(
+    <>
+      {loadImages()}
+    </>
+  )
+}
