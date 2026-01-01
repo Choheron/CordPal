05a36e59cce487108e44485924adfe14c12a6e02
Thomas Campbell <trc527@gmail.com>
2026-01-01 12:48:39 -0600

    Update banner

--- frontend/src/app/ui/playback/general/conditional_playback_banner.tsx
+++ frontend/src/app/ui/playback/general/conditional_playback_banner.tsx
@@ -14,6 +14,9 @@ export default async function CordpalPlaybackBanner(props) {
   const isJan = isJanuary()
   const globalPlaybackAvailable = await isPlaybackAvailable(today.getFullYear() - 1)
 
+  // Animated background tailwind
+  const animatedGradientTW = "animate-gradient-move bg-size-200 bg-gradient-to-tr from-violet-600 from-30% via-purple-800 via-50% to-violet-600 to-80%"
+
   return (
     <div className="hidden md:block fixed bottom-2 right-2 z-20">
       {/* Banner showing if global cordpal playback is available for last year*/}
@@ -21,7 +24,7 @@ export default async function CordpalPlaybackBanner(props) {
         <a
           href={`/playback/${today.getFullYear() - 1}`}
         >
-          <div className={`p-5 relative flex align-middle items-center snap-start bg-gradient-to-bl from-violet-600 to-purple-900 rounded-2xl ${bartle.className}`}>
+          <div className={`p-5 relative flex align-middle items-center snap-start ${animatedGradientTW} rounded-2xl ${bartle.className}`}>
             <div className="mx-auto">
               <p className="mx-auto text-2xl">CordPal Playback</p>
               <p className="text-right text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400">{today.getFullYear() - 1}</p>
