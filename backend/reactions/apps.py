4937ecb85b6571877342b8045c7cbb63684215ef
Thomas Campbell <trc527@gmail.com>
2026-05-02 17:54:48 -0500

    Fix useractions

--- backend/reactions/apps.py
+++ backend/reactions/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ReactionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reactions'
+
+    def ready(self):
+        import reactions.signals
